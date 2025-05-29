68563210cb761203d71d97673a959996d01fe4f8
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-05-29 22:43:56 +0300

    SehirListesi.cs

---
+++ SehirListesi.cs
@@ -0,0 +1,11 @@
+namespace ConsoleApp9
+{
+        public static class SehirListesi
+        {
+            public static List<string> Sehirler = new List<string>
+    {
+        "Gaziantep", "Ankara", "İstanbul", "İzmir", "Bursa", "Antalya", "Konya", "Adana", "Trabzon", "Balıkesir", "Tekirdağ"
+    };
+        }
+
+    }
