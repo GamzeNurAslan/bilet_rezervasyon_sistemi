12f836e32d0fe7244192ee05db6e929040252f53
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-05-29 22:43:30 +0300

    Koltuk.cs

---
+++ Koltuk.cs
@@ -0,0 +1,10 @@
+namespace ConsoleApp9
+{
+    public class Koltuk
+    {
+        public int No { get; set; }
+        public bool DoluMu { get; set; } = false;
+        public string YolcuAdi { get; set; }
+    }
+
+}
