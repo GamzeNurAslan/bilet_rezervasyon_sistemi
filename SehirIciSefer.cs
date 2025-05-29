19f515ed6998543fda85860d90cf1a331086d129
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-05-29 22:44:37 +0300

    SehirIciSefer.cs

---
+++ SehirIciSefer.cs
@@ -0,0 +1,9 @@
+namespace ConsoleApp9
+{
+        public class SehiriciSefer : Sefer
+        {
+            public SehiriciSefer(string kalkis, string varis, DateTime tarih, Otobus otobus)
+                : base(kalkis, varis, tarih, otobus) { }
+        }
+
+    }
