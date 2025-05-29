bf0855aba4f141f44efbf004498ce59db6b70b2e
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-05-29 22:41:27 +0300

    Otobus.cs

---
+++ Otobus.cs
@@ -0,0 +1,16 @@
+namespace ConsoleApp9
+{
+    public class Otobus
+    {
+        public string Plaka { get; set; }
+        public Koltuk[] Koltuklar { get; set; }
+
+        public Otobus(int koltukSayisi)
+        {
+            Koltuklar = new Koltuk[koltukSayisi];
+            for (int i = 0; i < koltukSayisi; i++)
+                Koltuklar[i] = new Koltuk { No = i + 1 };
+        }
+    }
+
+}
