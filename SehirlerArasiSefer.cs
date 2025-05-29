eb1ea364902714668d30b24a4454f8563fa90281
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-05-29 22:45:04 +0300

    SehirlerArasiSefer.cs

---
+++ SehirlerArasiSefer.cs
@@ -0,0 +1,9 @@
+namespace ConsoleApp9
+{
+    public class SehirlerarasiSefer : Sefer
+    {
+        public SehirlerarasiSefer(string kalkis, string varis, DateTime tarih, Otobus otobus)
+            : base(kalkis, varis, tarih, otobus) { }
+    }
+
+}
