8aa7c14bdc78d91e9406e1072a1f6034c9fe74f4
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-06-12 17:47:39 +0300

    Yolcu.cs

---
+++ Yolcu.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiBiletix
+{
+    public class Koltuk
+    {
+        public int Numara { get; set; }
+        public bool DoluMu { get; set; }
+    }
+}
